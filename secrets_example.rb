683528469520685333fae80dbbbb93dc24b48220
AlexanderKors <alexkors@ymail.com>
2023-11-15 15:06:03 -0800

    Update secrets_example.rb

--- secrets_example.rb
+++ secrets_example.rb
@@ -1,3 +1,3 @@
-USER = "your@email.com"
-PASS = "supersecretpassword"
-LOGIN_URL = ""
+USER = "alexkors@ymail.com"
+PASS = "Alex7CFIF35"
+LOGIN_URL = "https://sso.teachable.com/secure/76533/identity/login/password"
